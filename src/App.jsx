b03e51c8e9f8cbeab02e8841d20b0a23500e2630
Luispa23 <luis.pablo@bootcamp-upgrade.com>
2022-02-18 14:48:06 +0100

    Firs Commit: Api Works.. But isn't enough

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.scss";
+
+import Activity from "./components/Activity/Activity";
+
+const App = () => {
+
+ 
+
+  return (
+    <div className="App">
+      <Activity />
+    </div>
+  );
+}
+
+export default App;
